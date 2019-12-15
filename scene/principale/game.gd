6ec5b4fd988132b260627e3fe84bf560c77eacc5
TuxDave <godinodavide@gmail.com>
2019-12-15 20:12:45 +0100

    Implementata logica timer e record

--- scene/principale/game.gd
+++ scene/principale/game.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var game = 0
 var posizioni = []
 var posizioni2 = []
 var used = []
@@ -8,11 +9,15 @@ var time = 0
 var disabled = 0
 var faces = []
 var win = 0
+var timeStart = 0
+var timeActive = false
 
 func _ready():
 	scrumble()
 
 func _process(delta):
+	if timeActive:
+		timeUpdate(timeStart)
 	if $down.pressed:
 		$down.set_disabled(true)
 		disabled += 1
@@ -78,6 +83,24 @@ func _process(delta):
 		$record.hide()
 		$start.hide()
 		$scrumble.hide()
+		timeActive = false
+		isRecord($time.text, $record.text)
+		if Input.is_key_pressed(KEY_ESCAPE):
+			win = 0
+			$winLabel.hide()
+			$down.show()
+			$down2.show()
+			$up.show()
+			$up2.show()
+			$left.show()
+			$left2.show()
+			$right.show()
+			$right2.show()
+			$NinePatchRect.show()
+			$time.show()
+			$record.show()
+			$start.show()
+			$scrumble.show()
 
 func _on_scrumble_scrumble():
 	changeTime()
@@ -90,7 +113,6 @@ func changeTime():
 
 func scrumble():
 	seed(changeTime())
-	print(changeTime())
 	posizioni = [Vector2(125,50),
 				Vector2(250,50),
 				Vector2(375,50),
@@ -126,3 +148,53 @@ func scrumble():
 	$right2.set_disabled(false)
 	$left.set_disabled(false)
 	$left2.set_disabled(false)
+
+func _on_start_button_up():
+	timeStart = OS.get_time()
+	timeStart = timeStart["hour"] * 3600 + timeStart["minute"] * 60 + timeStart["second"]
+	timeActive = true
+
+func timeUpdate(rif):
+	var mytime = OS.get_time()["hour"] * 3600 + OS.get_time()["minute"] * 60 + OS.get_time()["second"]
+	var delta = mytime - rif
+	if delta > 3600:
+		mytime = [delta / 3600]
+		delta -= mytime[0] * 3600
+		mytime.append(delta / 60)
+		delta -= mytime[1] * 60
+		mytime.append(delta)
+	elif delta >= 60:
+		mytime = [0]
+		mytime.append(delta / 60)
+		delta -= mytime[1] * 60
+		mytime.append(delta)
+	else:
+		mytime = [0,0,delta]
+	mytime[0] = str(mytime[0])
+	mytime[1] = str(mytime[1])
+	mytime[2] = str(mytime[2])
+	if len(mytime[0]) == 1:
+		mytime[0] = "0" + mytime[0]
+	if len(mytime[1]) == 1:
+		mytime[1] = "0" + mytime[1]
+	if len(mytime[2]) == 1:
+		mytime[2] = "0" + mytime[2]
+	var timeString = "Time:\n" + mytime[0] + ":" + mytime[1] + ":" + mytime[2]
+	$time.text = timeString
+
+func isRecord(time, record):
+	var current = int(time[-8] + time[-7]) * 3600 + int(time[-5] + time[-4]) * 60 + int(time[-2] + time[-1])
+	var oldRecord = int(record[-8] + record[-7]) * 3600 + int(record[-5] + record[-4]) * 60 + int(record[-2] + record[-1])
+	if game == 0 or current < oldRecord:
+		record[-8] = time[-8]
+		record[-7] = time[-7]
+		record[-6] = time[-6]
+		record[-5] = time[-5]
+		record[-4] = time[-4]
+		record[-3] = time[-3]
+		record[-2] = time[-2]
+		record[-1] = time[-1]
+		$record.text = record
+	else:
+		$record.text = record
+	game += 1
